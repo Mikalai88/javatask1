0661070ada72d8cc86a45a77cb36d6e7151f413f
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2024-03-24 19:56:49 +0100

    task1 part2 (id)

--- src/main/java/epam/mikalai/array/entity/CustomArray.java
+++ src/main/java/epam/mikalai/array/entity/CustomArray.java
@@ -1,16 +1,21 @@
 package epam.mikalai.array.entity;
 
+import epam.mikalai.array.util.IdGenerator;
+
 import java.util.Arrays;
 
 public class CustomArray {
+    private final int customArrayId;
     private int[] customArray;
 
     public CustomArray() {
         this.customArray = new int[0];
+        this.customArrayId = IdGenerator.increment();
     }
 
     public CustomArray(int... customArray) {
         this.customArray = Arrays.copyOf(customArray, customArray.length);
+        this.customArrayId = IdGenerator.increment();
     }
 
     public int[] getCustomArray() {
@@ -19,5 +24,9 @@ public class CustomArray {
 
     public void setCustomArray(int[] customArray) {
         this.customArray = customArray;
+    }
+
+    public int getCustomArrayId() {
+        return customArrayId;
     }
 }
